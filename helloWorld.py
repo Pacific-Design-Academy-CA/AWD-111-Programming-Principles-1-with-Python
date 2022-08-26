aa24fda7adf04af3052a281599ff828f26bc9304
mahditasfin <64248085+mahditasfin@users.noreply.github.com>
2022-08-26 16:53:22 -0700

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import sys
+import datetime
+import calendar
+
+
+def helloWorld():
+    '''Print "Hello World"'''
+    
+
+
+def displayCurrentDateTime():
+   '''Print CurrentDateTime'''
+    
+
+def checkPythonVersion():
+    '''print Python Version'''
+    
+
+def printCalendar(year, month):
+    '''FREE QUESTION'''
+    '''Print Calender'''
+    
+if __name__ == '__main__':
+    pass
+
+
