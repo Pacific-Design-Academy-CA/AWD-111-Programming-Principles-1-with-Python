ee53d4bb289117764bc023264290e1e82ee3a542
mahditasfin <64248085+mahditasfin@users.noreply.github.com>
2022-08-26 17:05:10 -0700

    a3p1 & a3p2

---
+++ Assignment 3/a3p1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+
+def reverseString(str):
+    ''' Input: string -> String: The string that needs to be reversed '''
+    ''' Operation: Reverse the give string'''
+    ''' Output: If the string is "word",
+        drow
+    '''
+    ''' Associated Test Case: testReverseString()'''
+    pass
+   
+
+
+def printContentOfList(list):
+    ''' Input: a list with some elements'''
+    ''' Output: Print all the content of the list in separate lines'''
+    ''' Associated Test Case: testPrintContentOfList()'''
+    pass
+
+
+
+def printCommonStrings(list1,list2):
+    ''' Input: list1 & list2 are two lists with some strings'''
+    ''' Output: print all the common strings between two lists
+    separated by space,
+    list1 = ["m","s2","kq","js"]
+    list2 = ['ds','m','s2','kq','w']]
+    output: "m kq"
+    "'''
+    ''' Invalid Input: None'''
+    ''' Associated Test Case: testCalculateDigitsAndLettersInString'''
+    pass
+
+def listDivisibleByNumber(list1,number):
+    ''' Input: list1 & list2 are two lists with some strings'''
+    ''' Output: print all the common strings between two lists
+    separated by space,
+    list1 = ["m","s2","kq","js"]
+    list2 = ['ds','m','s2','kq','w']]
+    output: "m kq"
+    "'''
+    ''' Invalid Input: None'''
+    ''' Associated Test Case: testCalculateDigitsAndLettersInString'''
+    pass
+
+def uniqueElements(list1):  
+    # initialize a null list
+    pass
+    
+
+
+
+
+
+if __name__ == '__main__':
+    pass  
---
+++ Assignment 3/a3p2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+
+def filteredString(list1, list2):
+    ''' Input: string -> String: The string that needs to be reversed '''
+    ''' Operation: Reverse the give string'''
+    ''' Output: If the string is "word",
+        drow
+    '''
+    ''' Associated Test Case: testReverseString()'''
+    pass
+   
+
+
+def squaredValues(list):
+    ''' Input: a list with some elements'''
+    ''' Output: Print all the content of the list in separate lines'''
+    ''' Associated Test Case: testPrintContentOfList()'''
+    pass
+
+
+
+def checkPangram(str):
+    ''' Input: list1 & list2 are two lists with some strings'''
+    ''' Output: print all the common strings between two lists
+    separated by space,
+    list1 = ["m","s2","kq","js"]
+    list2 = ['ds','m','s2','kq','w']]
+    output: "m kq"
+    "'''
+    ''' Invalid Input: None'''
+    ''' Associated Test Case: testCalculateDigitsAndLettersInString'''
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    pass  
