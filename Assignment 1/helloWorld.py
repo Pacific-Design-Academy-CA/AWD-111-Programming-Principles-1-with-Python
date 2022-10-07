1170c622feac13268dcadc94ab07ce9fbecf747a
mahditasfin <64248085+mahditasfin@users.noreply.github.com>
2022-10-07 08:23:52 -0700

    Update helloWorld.py

--- Assignment 1/helloWorld.py
+++ Assignment 1/helloWorld.py
@@ -20,8 +20,25 @@ def checkPythonVersion():
 def printCalendar(year, month):
     '''FREE QUESTION'''
     '''Print Calender'''
+    print(calendar.month(year,month))
     
 if __name__ == '__main__':
-    pass
+
+    
+    #1 output of helloWorld()
+    print("\t#1 helloWorld()\t\n")
+    helloWorld()
+
+    #2 output of displayCurrentDateTime()
+    print("\t#2 displayCurrentDateTime()\t\n")
+    displayCurrentDateTime()
+
+    #3 output of checkPythonVersion()
+    print("\t#3 checkPythonVersion()\t\n")
+    checkPythonVersion()
+
+    #4 output of printCalendar()
+    print("\t#4 printCalendar()\t\n")
+    printCalendar(1999,12)
 
 
