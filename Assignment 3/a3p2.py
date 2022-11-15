adf64adb0dcaa9a5a5f012abf23ec007f65354ed
mahditasfin <64248085+mahditasfin@users.noreply.github.com>
2022-11-15 14:45:48 -0800

    Update a3p2.py

--- Assignment 3/a3p2.py
+++ Assignment 3/a3p2.py
@@ -2,34 +2,25 @@
 
 
 def filteredString(list1, list2):
-    ''' Input: string -> String: The string that needs to be reversed '''
-    ''' Operation: Reverse the give string'''
-    ''' Output: If the string is "word",
-        drow
-    '''
-    ''' Associated Test Case: testReverseString()'''
+    #get rid of the same values that are present in both list1 and list2
+    #create a list with the filtered values
+    #print them
     pass
    
 
 
 def squaredValues(list):
-    ''' Input: a list with some elements'''
-    ''' Output: Print all the content of the list in separate lines'''
-    ''' Associated Test Case: testPrintContentOfList()'''
+   #square the values in the list
+   #create a new list of squared values
+   #Print the list
     pass
 
 
 
 def checkPangram(str):
-    ''' Input: list1 & list2 are two lists with some strings'''
-    ''' Output: print all the common strings between two lists
-    separated by space,
-    list1 = ["m","s2","kq","js"]
-    list2 = ['ds','m','s2','kq','w']]
-    output: "m kq"
-    "'''
-    ''' Invalid Input: None'''
-    ''' Associated Test Case: testCalculateDigitsAndLettersInString'''
+    #given a string
+    #check if the string is a pangram, look up google for definition of pangram
+    #print "true" if pangram else "false"
     pass
 
 
@@ -37,4 +28,17 @@ def checkPangram(str):
 
 
 if __name__ == '__main__':
-    pass  
+    print("\nTest case for filtered(list, exception):\n\n case 1: a,s,e,q,f,ds; ds,s\n case 2: 0,12,102,0231,2,1; 0,1,12\n")
+    list1 = ['a','s','e','q', 'f', 'ds']
+    exceptList = ['ds','s']
+    filteredString(list1, exceptList)
+    print("\nTest case for squaredValues(list):\n\n case 1: 3,12,12,2,12\n case 2: 1,2,3,4,5\n")
+    list1=[3,12,12,2,12]
+    squaredValues(list1)
+    list1=[1,2,3,4,5]
+    squaredValues(list1)
+    print("\nTest case for checkPangram(pangram):\n\n case 1: The quick brown fox jumps over the lazy dog\n case 2: mars jupiter panda oc local marjuipter\n")
+    pangram = "The quick brown fox jumps over the lazy dog"
+    checkPangram(pangram)
+    pangram = "jupiter panda oc local marjuipter"
+    checkPangram(pangram)
